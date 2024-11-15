aca4342393254c41dc9c1092762fdbe55bc4e8d7
Eslan069 <sailelog.io@gmail.com>
2024-11-15 11:51:03 -0300

    Add Home Carousel

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,3 @@
-let nonee = document.getElementById('none');
-let none = document.querySelector('.none');
-
-none.addEventListener('click', function () {
-    let nameNone = document.documentElement.getAttribute('what');
-    let newName = (nameNone === 'what') ? 'what' : 'what';
-    document.documentElement.setAttribute('what', newName);
-});
-
-none.addEventListener('click', function () {
-    let none = document.documentElement.getAttribute('what');
-    let name = (none === 'what') ? 'what' : 'what';
-    document.documentElement.setAttribute('what', name);
-});
-
 const slider = document.querySelector('.slider');
 
 function activate(e) {
